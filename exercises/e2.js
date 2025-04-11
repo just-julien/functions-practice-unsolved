6482c451005ef9152c239d37474d4b098345a1f6
just-julien <jsaunders.cala23@gmail.com>
2025-04-11 10:51:57 -0500

    Modified exercise .js files to meet assignment criteria.

--- exercises/e2.js
+++ exercises/e2.js
@@ -4,14 +4,19 @@
 /**
  * ====================================================
  * Create a function that works as follows:
-   * The function name is 'rectArea'
-   * The function returns the value of the area of a rectangle (sideA * sideB)
-   * The function takes 2 parameters (two sides) to calculate the area from
+ * The function name is 'rectArea'
+ * The function returns the value of the area of a rectangle (sideA * sideB)
+ * The function takes 2 parameters (two sides) to calculate the area from
  * rectArea(3, 2) => 6
-*/
+ */
 
 // Your function code starts here...
 
+function rectArea(sideA, sideB) {
+  return sideA * sideB;
+}
+
+console.log(rectArea(3, 2));
 
 // Use the function you created above to calculate the areas of 3 rooms
 // Assign the results to room1, room2, and room3 variables
@@ -19,7 +24,13 @@
 
 // Your code goes here:
 
+const room1 = rectArea(4, 6);
+const room2 = rectArea(13, 3);
+const room3 = rectArea(4, 8);
 
+console.log(`Room 1 has an area of`, room1);
+console.log(`Room 3 has an area of`, room2);
+console.log(`Room 3 has an area of`, room3);
 
 /**
  * =============================================================
@@ -33,27 +44,29 @@
 
 // Your code goes here...
 
+function totalRoomsArea(area1, area2, area3) {
+  return area1 + area2 + area3;
+}
 
+const totalArea = totalRoomsArea(24, 35, 24);
+console.log(totalArea);
 
 // Make the function call in the parentheses of the console.log statement.
 // Use room1, room2, room3 variables as arguments for the function call
 const result = function () {
-    console.log(/* replace this comment with a function call here... */);
-}
-
-
+  console.log(totalRoomsArea(room1, room2, room3));
+};
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-2"
 // If the test has all tests passed, switch to the next exercise file
 // If any of the tests fails, refactor the code and run the test command after you've fixed the function
 
-
 // Do not delete or change the lines beneath
 const f = {
-    rectArea: rectArea || undefined,
-    roomVars: [room1 || undefined, room2 || undefined, room3 || undefined],
-    totalRoomsArea: totalRoomsArea || undefined,
-    result: (room1 && room2 && room3) ? result : undefined,
-}
+  rectArea: rectArea || undefined,
+  roomVars: [room1 || undefined, room2 || undefined, room3 || undefined],
+  totalRoomsArea: totalRoomsArea || undefined,
+  result: room1 && room2 && room3 ? result : undefined,
+};
 export { f };
--- exercises/e5.js
+++ exercises/e5.js
@@ -1,8 +1,7 @@
-// In this exercise you'll complete a series of "comparison" mini exercises. 
-// Conditional logic is a huge part of coding. It's what helps a program make decisions. 
+// In this exercise you'll complete a series of "comparison" mini exercises.
+// Conditional logic is a huge part of coding. It's what helps a program make decisions.
 // Finish each exercise below.
 // You can use ANY type of function syntax you want: named, arrow, or expression with anonymous function.
-
 
 /** =========================
  * Create a getDiffTwentySeven(num) function to return the difference between a given number and 27
@@ -13,8 +12,16 @@
 
 // Your code goes here...
 
+function getDiffTwentySeven(num) {
+  const diff = Math.abs(num - 27);
+  if (num > 27) {
+    return diff * 2;
+  } else {
+    return diff;
+  }
+}
 
-
+console.log(getDiffTwentySeven);
 
 /** =========================
  * Create a sumOfTwoIntegers(num1, num2) function to compute and return the sum of the two given integers
@@ -25,8 +32,16 @@
 
 // Your code goes here
 
+function sumOfTwoIntegers(num1, num2) {
+  const sum = num1 + num2;
+  if (num1 === num2) {
+    return sum * 3;
+  } else {
+    return sum;
+  }
+}
 
-
+console.log(sumOfTwoIntegers);
 
 /** =========================
  * Create a isOneOfThemOrTheirSumIsFourty(num1, num2) function to check two given numbers and return a boolean.
@@ -39,19 +54,25 @@
 
 // Your code goes here...
 
+function isOneOfThemOrTheirSumIsFourty(num1, num2) {
+  if (num1 === 40 || num2 === 40 || num1 + num2 === 40) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-
+console.log(isOneOfThemOrTheirSumIsFourty);
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-5"
 // If the test has all tests passed, switch to the next exercise file
 // If any of the tests fails, refactor the code and run the test command after you've fixed the function
 
-
 // Do not delete or change the lines beneath
 const f = {
   getDiffTwentySeven: getDiffTwentySeven || undefined,
   sumOfTwoIntegers: sumOfTwoIntegers || undefined,
   isOneOfThemOrTheirSumIsFourty: isOneOfThemOrTheirSumIsFourty || undefined,
-}
-export { f }
+};
+export { f };
